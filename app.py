dcd03994cebeeb36d88bb2754cf22e6b48eae396
mishratushar739-a11y <mishratushar739@gmail.com>
2026-06-21 22:55:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def get_connection_params():
         secrets_mysql = {}
 
     return {
-        "host": secrets_mysql.get("host", os.environ.get("MYSQL_HOST", "mysql.railway.internal")),
+        "host": secrets_mysql.get("host", os.environ.get("MYSQL_HOST", "localhost")),
         "port": secrets_mysql.get("port", os.environ.get("MYSQL_PORT", 3306)),
         "user": secrets_mysql.get("user", os.environ.get("MYSQL_USER", "root")),
         "password": secrets_mysql.get("password", os.environ.get("MYSQL_PASSWORD", "")),
